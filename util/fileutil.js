a545ef99f748a9830f435094fafdda11b1736155
陆英杰 <34340797+shiniaxiaomi@users.noreply.github.com>
2019-09-02 00:42:49 +0800

    bc

--- util/fileutil.js
+++ util/fileutil.js
@@ -15,13 +15,13 @@ function buildIndexHtml(srcDir, targetDir, dirHtml, staticPath) {
   try {
     fs.writeFileSync(
       path.join(targetDir, "index.html"),
-      indexHtml
-      // minify(indexHtml, {
-      //   removeComments: true,
-      //   collapseWhitespace: true,
-      //   minifyJS: true,
-      //   minifyCSS: true
-      // })
+      // indexHtml
+      minify(indexHtml, {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        minifyCSS: true
+      })
     ); //开启文本压缩
   } catch (err) {
     console.log("html文件写入失败:" + err);
