2b7db38ae24cf0bd381a1817f823ae358e7902eb
Yingjie.Lu <Yingjie.Lu@geely.com>
2019-09-09 20:17:26 +0800

    添加定时任务定时更新笔记

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express"); //导入express模块
 var bodyParser = require("body-parser");
+var schedule = require('node-schedule');//导入定时任务模块
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var app = express(); //获取app对象
@@ -7,18 +8,18 @@ const path = require("path");
 const build = require("./build"); //获取构建对象
 
 //linux
-// var gitUrl = "https://github.com/shiniaxiaomi/note.git"; //git网址
-// var srcDir = "/note"; //原笔记存放路径
-// var targetDir = "/html"; //html生成路径
-// var staticPath = "http://47.105.165.211"; //静态资源路径
+var gitUrl = "https://github.com/shiniaxiaomi/note.git"; //git网址
+var srcDir = "/note"; //原笔记存放路径
+var targetDir = "/html"; //html生成路径
+var staticPath = "http://47.105.165.211"; //静态资源路径
 
 //本地
-var gitUrl = "https://github.com/shiniaxiaomi/mdToHtml.git";
-// var srcDir = "C:\\Users\\Administrator\\Desktop\\note";
-var srcDir = "C:\\Users\\yingjie.lu\\Desktop\\note";
-// var targetDir = "C:\\Users\\Administrator\\Desktop\\html";
-var targetDir = "C:\\Users\\yingjie.lu\\Desktop\\html";
-var staticPath = "http://localhost";
+// var gitUrl = "https://github.com/shiniaxiaomi/mdToHtml.git";
+// // var srcDir = "C:\\Users\\Administrator\\Desktop\\note";
+// var srcDir = "C:\\Users\\yingjie.lu\\Desktop\\note";
+// // var targetDir = "C:\\Users\\Administrator\\Desktop\\html";
+// var targetDir = "C:\\Users\\yingjie.lu\\Desktop\\html";
+// var staticPath = "http://localhost";
 
 //构建笔记html
 var buildOutput = build.startToBuild(gitUrl, srcDir, targetDir, staticPath);
@@ -56,3 +57,17 @@ app.post("/syncNote", urlencodedParser, function(req, res) {
     res.send({ flag: 0, data: "同步密码错误" }); //失败
   }
 });
+
+
+//添加一个每天凌晨定时拉去更新笔记的功能
+//每天凌晨4点钟执行
+schedule.scheduleJob('0 0 4 * * *', function(){
+  console.log("定时任务开始构建笔记--------------");
+  var output = build.startToBuild(gitUrl, srcDir, targetDir, staticPath);
+
+  if (output.flag) {
+    console.log("定时任务构建笔记成功!");
+  } else {
+    console.log("定时任务构建笔记失败:"+output.data);
+  }
+}); 
--- test.js
+++ test.js
@@ -1,15 +1,22 @@
-function genID(length) {
-  return Number(
-    Math.random()
-      .toString()
-      .substr(3, length) + new Date().getTime()
-  ).toString(36);
-}
+// function genID(length) {
+//   return Number(
+//     Math.random()
+//       .toString()
+//       .substr(3, length) + new Date().getTime()
+//   ).toString(36);
+// }
 
-for (var i = 1; i < 10; i++) {
-  console.log(
-    Math.random()
-      .toString()
-      .substr(3, 15)
-  );
-}
+// for (var i = 1; i < 10; i++) {
+//   console.log(
+//     Math.random()
+//       .toString()
+//       .substr(3, 15)
+//   );
+// }
+
+var schedule = require('node-schedule');//导入定时任务模块
+
+//每天凌晨4点钟执行
+schedule.scheduleJob('0 0 4 * * *', function(){
+  console.log('scheduleCronstyle:' + new Date());
+}); 
