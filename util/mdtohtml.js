c2d086ca9de9ed4bcdfa20921f550463b70eac13
陆英杰 <34340797+shiniaxiaomi@users.noreply.github.com>
2019-09-15 01:30:12 +0800

    auto commit

--- util/mdtohtml.js
+++ util/mdtohtml.js
@@ -102,10 +102,10 @@ exports.startToBuild = function(srcDir, targetDir, staticPath) {
 
       //解析文件并生成html
       var noteHtml = marked(noteStr, { renderer: renderer });
-      noteHtml = noteHtml.replace(
-        new RegExp('class="javascript"', "gm"),
-        'class="js"'
-      ); //将class为JavaScript的替换成js
+      // noteHtml = noteHtml.replace(
+      //   new RegExp('class="javascript"', "gm"),
+      //   'class="js"'
+      // ); //将class为JavaScript的替换成js
       //生成toc目录
       var tocHtml = fileUtil.getTocHtml(titleList);
 
